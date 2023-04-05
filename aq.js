8eeec27d6566721ca521dd7d9f4657a83d5060c2
anish-mankani1 <mankanianeesh@gmail.com>
2023-04-05 14:01:40 +0530

    js

---
+++ aq.js
@@ -0,0 +1,31 @@
+// chp 1 questions
+// q1 create a string  and try to add a number on it 
+let a="harrry"
+let b=6
+console.log(a+b)
+//q2 find the type of operator of a and b
+console.log( typeof (a+b))
+// q3 try to add a new key to the constant object
+const a1=
+{
+    name:"shubham",
+    height:"anish",
+    weight:"sumit",
+    kudi:65
+}
+console.log(a1["name"])
+console.log(a1["kudi"])
+a1['friend']="rohit"
+console.log(a1["friend"])
+
+// q4 write a program to create a word meaning dictionary of five words
+const dict =
+{
+    appreciative:" helping",
+    yakka:"helpful",
+    pukka:"helpless"
+
+}
+console.log(dict.yakka)
+console.log(dict['appreciative'])
+console.log(dict["pukka"])
